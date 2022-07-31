8ef7660a0857c74d3c3c27fe7662b32baa6b7305
Alexandre Gibault <ag-mainmail@protonmail.com>
2022-07-31 13:44:51 +0200

    Add /first-project and /variables files

---
+++ variables/script.js
@@ -0,0 +1,28 @@
+"use-stric";
+
+//Use "let variableName;" to declare a variable named "variableName"
+let admin;
+let name;
+
+//Value assignment to a variable
+name = "John";
+admin = name;
+
+//Example to use a variable
+alert(admin);
+
+//Note tou can assign a value directly in the declaration line
+let intValue = 1;
+
+/*
+Use "const constantName;" to declare a constant named "constantName".
+The value of a constant cannot be modified once initialized.
+Uppercase the constant name when the value is set in the source code.
+Otherwise, the constant name is lowercase.
+*/
+const BIRTHDAY = '12.24.1982';
+const age = age(BIRTHDAY);
+
+//Use camel case typing to name a variable !
+
+//TODO age function
