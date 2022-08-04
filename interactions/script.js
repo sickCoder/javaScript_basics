3d3cbb58633dd21e844931f4251c20356f80ac3a
Alexandre Gibault <ag-mainmail@protonmail.com>
2022-08-04 14:08:40 +0200

    Update interactions/script.js

--- interactions/script.js
+++ interactions/script.js
@@ -1,7 +1,15 @@
 "user-strict";
 
+//Pops a modal window with an entry field
 let name = prompt("What is your name?", "");
+//Pops a modal window with a message
 alert(name);
 
 //Works too, but harder to read and understand
 //alert(prompt("What is your name?", ""));
+/*
+Pops a modal window with OK and Cancel buttons
+OK returns true, Cancel or escape returns false
+*/
+let isBoss = confirm("Are you the boss?");
+alert(isBoss);
