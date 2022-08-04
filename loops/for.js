e22a4591f830cb2225f002e27f801a6a5b303b60
Alexandre Gibault <ag-mainmail@protonmail.com>
2022-08-04 13:46:37 +0200

    Udate loops/for.js

--- loops/for.js
+++ loops/for.js
@@ -1,12 +1,12 @@
 "use-strict";
 
-for (let i = 0; i <= 10; i++) {
+for (let i = 2; i <= 10; i++) {
     if (i % 2 == 0) 
         alert(i);    
 }
 /*
 //Another way
-for (let i = 0; i <= 10; i++) {
+for (let i = 2; i <= 10; i++) {
     if (i % 2 != 0)
         continue;
     alert(i);
