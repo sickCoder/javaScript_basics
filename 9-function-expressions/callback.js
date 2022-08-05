af749004a3178742465b06b5a3ec5819f216685f
Alexandre Gibault <ag-mainmail@protonmail.com>
2022-08-05 14:56:28 +0200

    Add 9-function-expressions/callback.js

---
+++ 9-function-expressions/callback.js
@@ -0,0 +1,38 @@
+"use-stric";
+
+/* 
+question = question text
+yes = function to execute in case yes
+no = function to execute in case no
+*/
+function ask(question, yes, no) {
+    if(confirm(question))
+        yes();
+    else
+        no();
+}
+
+function showOk() {
+    alert('You agreed.');
+}
+
+function showCancel() {
+    alert('Execution canceled.');
+}
+/*
+Using showOk and showCancel as callbacks functions.
+One of them will be called later in the function ask execution.
+*/
+ask("Do you agree?", showOk, showCancel);
+
+/*
+We could also call ask with anonymous functions.
+These anonymous functions cannot be accessed outside
+of ask becouse they are not into variables.
+However in this particular case, this is exactly what we need.
+*/
+ask(
+    "Do you agree?",
+    function () { alert('You agreed.'); },
+    function () { alert('Execution canceled'); }
+);
