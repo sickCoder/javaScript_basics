34e519d43a2fb09021b328b7c5a0166f9a07cded
Alexandre Gibault <ag-mainmail@protonmail.com>
2022-08-08 15:43:03 +0200

    Update 11-tests/test.js

--- 11-tests/test.js
+++ 11-tests/test.js
@@ -2,8 +2,12 @@
 
 describe("pow", function() {
 
-    it("raises to n-th power", function() {
+    it("2 raised to power 3 is 8", function() {
         assert.equal(pow(2, 3), 8);
     });
-    
+    //In case of the first it fails, the second it will be executed
+    it("3 raised to power 4 is 81", function() {
+        assert.equal(pow(3, 4), 81);
+    });
+
 });
