5e35fe86aa81377b107f29c5c17844e50a7bfdb9
Alexandre Gibault <ag-mainmail@protonmail.com>
2022-08-09 16:37:05 +0200

    Add 012-objects/001-basics/multiply.js

---
+++ 012-objects/001-Basics/multiply.js
@@ -0,0 +1,18 @@
+"use-strict";
+
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+};
+
+multiplyNumeric(menu);
+
+//Function that's only doubles the numeric properties
+function multiplyNumeric(object) {
+    for (let key in object){
+        if (typeof object[key] == 'number') {
+            object[key] *= 2;
+        }
+    }
+}
--- 012-objects/001-Basics/sum.js
+++ 012-objects/001-Basics/sum.js
@@ -4,7 +4,7 @@ let salaries = {
     John: 100,
     Ann: 160,
     Pete: 130
-}
+};
 
 let sum = getTotalSalaries(salaries);
 
