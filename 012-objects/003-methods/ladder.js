63e77abcc75443611b5ae0d77925cc1d24d66963
Alexandre Gibault <ag-mainmail@protonmail.com>
2022-08-10 16:25:44 +0200

    Add 012-objects/003-methods/ladder.js

---
+++ 012-objects/003-methods/ladder.js
@@ -0,0 +1,25 @@
+"use-strict";
+
+let ladder = {
+    step: 0,
+    up() {
+        this.step++;
+        return this;
+    },
+    down() {
+        this.step--;
+        return this;
+    },
+    showStep() {
+        alert(this.step);
+        return this.step;
+    }
+};
+
+ladder
+    .up()
+    .up()
+    .down()
+    .showStep()
+    .down()
+    .showStep();
