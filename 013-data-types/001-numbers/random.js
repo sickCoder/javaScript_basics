974da3c8d5ab257a3ef4201f11e59fdf57b7492d
Alexandre Gibault <ag-mainmail@protonmail.com>
2022-08-15 13:22:06 +0200

    Update 013-data-types/001-numbers/random.js

--- 013-data-types/001-numbers/random.js
+++ 013-data-types/001-numbers/random.js
@@ -6,3 +6,11 @@ function random(min, max) {
 
 for (let i = 0; i < 3; i++) 
     alert(random(1, 5));
+
+function randomInteger(min, max) {
+    let random = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(random);
+}
+
+for (let i = 0; i < 3; i++)
+    alert(randomInteger(1, 5));
