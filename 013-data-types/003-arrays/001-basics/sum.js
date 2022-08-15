05a3b23094576d118635e48d8d494e1b35ba7e35
Alexandre Gibault <ag-mainmail@protonmail.com>
2022-08-15 16:39:23 +0200

    Update 013-data-types/003-arrays/001-basics/sum.js

--- 013-data-types/003-arrays/001-basics/sum.js
+++ 013-data-types/003-arrays/001-basics/sum.js
@@ -2,17 +2,16 @@
 
 function sumInput() {
     let numbers = [];
-    let value = 0;
 
     do {
-        
-        value = +prompt("Enter a number:", 0);
+
+        let value = prompt("Enter a number:", 0);
         
         if (!isFinite(number) || value == "" || value == null) {
             break;
         }
 
-        numbers.push(value);
+        numbers.push(+value);
     } while (true);
     
     let sum = 0;
