a9f554ae61c9a58005354eaad06656db77e4c1f4
Alexandre Gibault <ag-mainmail@protonmail.com>
2022-08-16 16:04:16 +0200

    Update 013-data-types/003-arrays/002-methods/filterRange.js

--- 013-data-types/003-arrays/002-methods/filterRange.js
+++ 013-data-types/003-arrays/002-methods/filterRange.js
@@ -1,5 +1,17 @@
 "use-strict";
 
-function filterRange(array, a, b) {
-    return array.filter(item => item >= a && item <= b);
+function filterRange(array, min, max) {
+    return array.filter(item => item >= min && item <= max);
+}
+
+function rangeInPlace(array, min, max) {
+
+    for (let i = 0; i < array.length; i++) {
+        let currentValue = array[i];
+
+        if (currentValue < min || currentValue > max) {
+            array.splice(i, 1);
+            i--;
+        }
+    }
 }
