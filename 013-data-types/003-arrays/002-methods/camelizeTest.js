98a16ea50148558e017525cf5325fb4a59efa446
Alexandre Gibault <ag-mainmail@protonmail.com>
2022-08-16 15:16:01 +0200

    Add 0013-data-types/003-arrays/002-methods/camelizeTeset.js

---
+++ 013-data-types/003-arrays/002-methods/camelizeTest.js
@@ -0,0 +1,19 @@
+"use-strict";
+
+describe(camelize, function() {
+    
+    it("camel case a hypen formatted string", function() {
+        let str = "background-color";
+        assert.equal(camelize(str), "backgroundColor");
+    });
+
+    it("camel case a multiword hypen formatted string", function() {
+        let str = "list-style-image";
+        assert.equal(camelize(str), "listStyleImage");
+    });
+
+    it("camel case a hyphen formatted string starting with a hyphen", function() {
+        let str = "-webkit-transition";
+        assert.equal(camelize(str), "WebkitTransition");
+    });
+});
