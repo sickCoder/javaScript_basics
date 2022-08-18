f0e9edea9999575fe638508fa09970a51e553e71
Alexandre Gibault <ag-mainmail@protonmail.com>
2022-08-18 17:37:07 +0200

    Add 013-data-types/008-dates/secondsToTomorrow.js

---
+++ 013-data-types/008-dates/secondsToTomorrow.js
@@ -0,0 +1,8 @@
+"use-strict";
+
+function getSecondsToTomorrow() {
+    let now = new Date();
+    let tomorrow = new Date(now.getFullYear(), now.getMonth(). now.getDate() + 1);
+
+    return Math.round((tomorrow - now) / 1000);
+}
